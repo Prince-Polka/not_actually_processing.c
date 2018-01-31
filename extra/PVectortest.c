9b822bfba8c1e98b2db34e38a5cf2a2cced9e8aa
Prince-Polka <andre.hallqvist@gmail.com>
2018-01-31 19:18:29 +0100

    Update PVectortest.c

--- extra/PVectortest.c
+++ extra/PVectortest.c
@@ -1,16 +1,10 @@
-/* test it @ http://rextester.com/HWR73007 */
+/* test it @ http://rextester.com/LBLM44761 */
 #include <stdio.h>
 /* the data must be packed and aligned, otherwise assembly may cause sigsev */
 typedef struct PVector {double x,y,z,w;} __attribute__ ((aligned (32),packed)) PVector;
 
 /* AVX macro that makes a AT&T syntax*/
-#define AVX(function,mnemonic) \
-PVector static inline function \
-(PVector A, PVector B){PVector ret; asm( \
-"VMOVDQA %1,%%ymm1;\n" \
-#mnemonic " %2,%%ymm1,%%ymm0;\n" \
-"VMOVDQA %%ymm0,%0;\n" \
-: "=m" (ret): "m" (A) ,"m" (B));return ret;}
+
 /*
 first argument name of funtion to be created by macro,
 second argument the x86 mnemonic
@@ -18,28 +12,56 @@ mnemonics can be found at the right-hand side of this page
 https://software.intel.com/sites/landingpage/IntrinsicsGuide/
 */
 
-/* this makes functions for add subtract multiply and divide of doubles */
-AVX(add,VADDPD)
-AVX(sub,VSUBPD)
-AVX(mult,VMULPD)
-AVX(div,VDIVPD)
+/* only one pvector argument */
 
-AVX(addsub,VADDSUBPD)
-    
-// AVX(sqrt,VSQRTPD) only one argument
+#define AVXA(function,mnemonic) \
+PVector static inline function \
+(PVector A){PVector ret; asm( \
+#mnemonic " %1,%%ymm0;\n" \
+"VMOVDQA %%ymm0,%0;\n" \
+: "=m" (ret): "m" (A));return ret;}
 
-AVX(min,VPMIND)
-AVC(max,VPMAXD)
-    
+AVXA(pvsqrt,VSQRTPD) 
+
+/* one pvector and value 0-3, round floor ceil trunc*/
+
+#define AVXR(function,imm8) \
+PVector static inline function \
+(PVector A){PVector ret; char imm = 1;  asm( \
+"VROUNDPD $" #imm8 ",%1,%%ymm0;\n" \
+"VMOVDQA %%ymm0,%0;\n" \
+: "=m" (ret): "m" (A), "i" (imm) );return ret;}
+
+AVXR(pvround,0)
+AVXR(pvfloor,1)
+AVXR(pvceil,2)
+AVXR(pvtrunc,3)
+
+/* two pvector argument */
+
+#define AVXAB(function,mnemonic) \
+PVector static inline function \
+(PVector A, PVector B){PVector ret; asm( \
+"VMOVDQA %1,%%ymm1;\n" \
+#mnemonic " %2,%%ymm1,%%ymm0;\n" \
+"VMOVDQA %%ymm0,%0;\n" \
+: "=m" (ret): "m" (A) ,"m" (B));return ret;}
+
+AVXAB(add,VADDPD)
+AVXAB(sub,VSUBPD)
+AVXAB(mult,VMULPD)
+AVXAB(div,VDIVPD)
+
+//untested
+//AVXAB(addsub,VADDSUBPD)
+//AVXAB(min,VPMIND)
+//AVXAB(max,VPMAXD)
 // bitwise
-AVX(and,VANDPD)
-AVX(andnot,VANDNPD)
-AVX(xor,VORPD)
-AVX(xor,VXORPD)
-//AVX(ceil,VROUNDPD) imm8
-//AVX(floor,VROUNDPD) imm8
+//AVXAB(pvand,VANDPD)
+//AVXAB(pvandnot,VANDNPD)
+//AVXAB(pvor,VORPD)
+//AVXAB(pvxor,VXORPD)
 
-    
 /* convenience function to make or set a PVector */
 PVector new_PVector(double X,double Y,double Z, double W){
     PVector ret;
@@ -57,7 +79,7 @@ void print_PVector(PVector PV){
 }
 
 int main(void){
-    PVector a, b;
+    PVector a, b, c;
     a = new_PVector(4,5,6,7);
     b = new_PVector(1,2,3,4);
     
@@ -66,4 +88,13 @@ int main(void){
     print_PVector( div(a,b) );
     
     print_PVector( mult(a,b) );
+
+    print_PVector( pvsqrt(b) );
+    
+    c = new_PVector(-1.1, 2.3, -3.6, 4.7);
+    
+    print_PVector( pvround(c) );
+    print_PVector( pvfloor(c) );
+    print_PVector( pvceil(c) );
+    print_PVector( pvtrunc(c) );
 }
